d3b643a56eacd84495ebd6184f699e082a8d98fc
kdoore <kdoore@gmail.com>
2014-11-18 22:13:00 +0000

    ColorWheel
    Rename colorwheel html file

---
+++ js/sketch4.js
@@ -0,0 +1,25 @@
+
+setup=function(){
+    background(255);
+    var canvas1=createCanvas(400,400);
+    canvas1.parent('container1');
+    
+    
+    
+}
+
+
+
+draw=function(){
+    background(255);
+    noStroke();
+    fill(0);
+    textSize(14);
+    text("Let's create some designs", 100,100);
+    strokeWeight(3);
+    stroke(100);
+    fill(100,200,200);
+    ellipse(200,200,50,50);
+    
+    
+}
---
+++ js/sketch5.js
@@ -0,0 +1,11 @@
+//declare global variables
+//setup to initialize global variables and call functions which are only executed once
+setup = function() {
+    var div1 = createDiv('');
+    div1.id = "canvasContainer";
+    var canvas1 = createCanvas(400, 400);
+    canvas1.parent('canvasContainer');
+}
+draw = function() {
+    
+}
